a4024e8094e3bf39e42b215e941bebe74e50da07
Ed Kelly <edwardjkelly@gmail.com>
2023-04-10 10:17:47 +0100

    Add custom interface to ToString

--- examples/src/Tools/ToString.elm
+++ examples/src/Tools/ToString.elm
@@ -4,10 +4,14 @@ module Tools.ToString exposing (interface)
 interface =
     { record = strRecord
     , field = strField
-    , end = strEnd
+    , endRecord = strEnd
     , string = strString
     , int = strInt
     , bool = strBool
+    , custom = strCustom
+    , tag0 = strTag0
+    , tag1 = strTag1
+    , endCustom = strEndCustom
     }
 
 
@@ -25,6 +29,22 @@ strField fieldName getField toString builder =
 strEnd : (recordData -> List String) -> recordData -> String
 strEnd builder =
     \recordData -> "{ " ++ String.join ", " (builder recordData |> List.reverse) ++ " }"
+
+
+strCustom dtor =
+    dtor
+
+
+strTag0 tagName tagCtor dtor =
+    dtor tagName
+
+
+strTag1 tagName tagCtor child dtor =
+    dtor (\c -> tagName ++ " " ++ child c)
+
+
+strEndCustom dtor =
+    dtor 
 
 
 strString : String -> String
