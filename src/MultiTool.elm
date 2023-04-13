08b18ec60cae497016de444a67ec536e273cd5bf
Ed Kelly <edwardjkelly@gmail.com>
2023-04-13 13:19:47 +0100

    Add type annotations

--- src/MultiTool.elm
+++ src/MultiTool.elm
@@ -488,159 +488,191 @@ end toolBuilder =
     }
 
 
+doMakeList : (({} -> {} -> {}) -> c -> d -> e) -> c -> d -> e
 doMakeList listMaker_ listChildren_ lists_ =
     listMaker_ (\{} {} -> {}) listChildren_ lists_
 
 
+listMaker : (b -> a -> c) -> ( d, b ) -> ( d -> e, a ) -> ( e, c )
 listMaker next ( listChild, restListChildren ) ( list_, restLists ) =
     ( list_ listChild
     , next restListChildren restLists
     )
 
 
+doMakeMaybe : (({} -> {} -> {}) -> c -> d -> e) -> c -> d -> e
 doMakeMaybe maybeMaker_ maybeContents_ maybes_ =
     maybeMaker_ (\{} {} -> {}) maybeContents_ maybes_
 
 
+maybeMaker : (b -> a -> c) -> ( d, b ) -> ( d -> e, a ) -> ( e, c )
 maybeMaker next ( maybeContent, restMaybeContents ) ( maybe_, restMaybes ) =
     ( maybe_ maybeContent
     , next restMaybeContents restMaybes
     )
 
 
+doMakeArray : (({} -> {} -> {}) -> c -> d -> e) -> c -> d -> e
 doMakeArray maker_ contents_ containers_ =
     maker_ (\{} {} -> {}) contents_ containers_
 
 
+arrayMaker : (b -> a -> c) -> ( d, b ) -> ( d -> e, a ) -> ( e, c )
 arrayMaker next ( content, restContents ) ( array_, restArrays ) =
     ( array_ content
     , next restContents restArrays
     )
 
 
+doMakeDict : (({} -> {} -> {} -> {}) -> d -> e -> f -> g) -> d -> e -> f -> g
 doMakeDict dictMaker_ keys_ values_ dicts_ =
     dictMaker_ (\{} {} {} -> {}) keys_ values_ dicts_
 
 
+dictMaker : (b -> a -> c -> d) -> ( e, b ) -> ( f, a ) -> ( e -> f -> g, c ) -> ( g, d )
 dictMaker next ( key_, restKeys ) ( value_, restValues ) ( dict_, restDicts ) =
     ( dict_ key_ value_
     , next restKeys restValues restDicts
     )
 
 
+doMakeSet : (({} -> {} -> {}) -> c -> d -> e) -> c -> d -> e
 doMakeSet setMaker_ contents_ sets =
     setMaker_ (\{} {} -> {}) contents_ sets
 
 
+setMaker : (b -> a -> c) -> ( d, b ) -> ( d -> e, a ) -> ( e, c )
 setMaker next ( content, restContents ) ( set_, restSets ) =
     ( set_ content
     , next restContents restSets
     )
 
 
+doMakeTuple : (({} -> {} -> {} -> {}) -> d -> e -> f -> g) -> d -> e -> f -> g
 doMakeTuple tupleMaker_ a b tuples =
     tupleMaker_ (\{} {} {} -> {}) a b tuples
 
 
+tupleMaker : (b -> a -> c -> d) -> ( e, b ) -> ( f, a ) -> ( e -> f -> g, c ) -> ( g, d )
 tupleMaker next ( a, restAs ) ( b, restBs ) ( tuple_, restTuples ) =
     ( tuple_ a b
     , next restAs restBs restTuples
     )
 
 
+doMakeTriple : (({} -> {} -> {} -> {} -> {}) -> e -> f -> g -> h -> i) -> e -> f -> g -> h -> i
 doMakeTriple tripleMaker_ a b c triples =
     tripleMaker_ (\{} {} {} {} -> {}) a b c triples
 
 
+tripleMaker : (b -> a -> c -> d -> e) -> ( f, b ) -> ( g, a ) -> ( h, c ) -> ( f -> g -> h -> i, d ) -> ( i, e )
 tripleMaker next ( a, restAs ) ( b, restBs ) ( c, restCs ) ( triple, restTriples ) =
     ( triple a b c
     , next restAs restBs restCs restTriples
     )
 
 
+doMakeResult : (({} -> {} -> {} -> {}) -> d -> e -> f -> g) -> d -> e -> f -> g
 doMakeResult resultMaker_ errors values results =
     resultMaker_ (\{} {} {} -> {}) errors values results
 
 
+resultMaker : (b -> a -> c -> d) -> ( e, b ) -> ( f, a ) -> ( e -> f -> g, c ) -> ( g, d )
 resultMaker next ( error, restErrors ) ( value, restValues ) ( result, restResults ) =
     ( result error value
     , next restErrors restValues restResults
     )
 
 
+doMakeRecord : ((a -> {} -> {}) -> c -> d -> e) -> c -> d -> e
 doMakeRecord recordMaker_ recordConstructor records =
     recordMaker_ (\_ {} -> {}) recordConstructor records
 
 
+recordMaker : (b -> a -> c) -> b -> ( b -> d, a ) -> ( d, c )
 recordMaker next recordConstructor ( record_, records ) =
     ( record_ recordConstructor
     , next recordConstructor records
     )
 
 
+doMakeFields : ((a -> b -> {} -> {} -> {} -> {}) -> f -> g -> h -> i -> j -> k) -> f -> g -> h -> i -> j -> k
 doMakeFields fieldMaker_ fieldName getField child recordBuilders fields =
     fieldMaker_ (\_ _ {} {} {} -> {}) fieldName getField child recordBuilders fields
 
 
+fieldMaker : (b -> c -> a -> d -> e -> f) -> b -> c -> ( g, a ) -> ( h, d ) -> ( b -> c -> g -> h -> i, e ) -> ( i, f )
 fieldMaker next fieldName getField ( child, restChilds ) ( builder, restBuilders ) ( field_, restFields ) =
     ( field_ fieldName getField child builder
     , next fieldName getField restChilds restBuilders restFields
     )
 
 
+doEndRecord : (({} -> {} -> {}) -> c -> d -> e) -> c -> d -> e
 doEndRecord recordEnder_ builder endRecords =
     recordEnder_ (\{} {} -> {}) builder endRecords
 
 
+recordEnder : (b -> a -> c) -> ( d, b ) -> ( d -> e, a ) -> ( e, c )
 recordEnder next ( builder, restBuilders ) ( endRecord_, restEndRecords ) =
     ( endRecord_ builder
     , next restBuilders restEndRecords
     )
 
 
+doMakeCustom : ((a -> {} -> {} -> {}) -> d -> e -> f -> g) -> d -> e -> f -> g
 doMakeCustom customMaker_ customDestructors destructorFieldGetters customs =
     customMaker_ (\_ {} {} -> {}) customDestructors destructorFieldGetters customs
 
 
+customMaker : (b -> a -> c -> d) -> b -> ( b -> e, a ) -> ( e -> f, c ) -> ( f, d )
 customMaker next customDestructors ( destructorFieldGetter, restDestructorFieldGetters ) ( custom, restCustoms ) =
     ( custom (destructorFieldGetter customDestructors)
     , next customDestructors restDestructorFieldGetters restCustoms
     )
 
 
+doMakeTag0 : ((a -> b -> {} -> {} -> {}) -> e -> f -> g -> h -> i) -> e -> f -> g -> h -> i
 doMakeTag0 tag0Maker_ tagName tagConstructor customBuilder tag0s =
     tag0Maker_ (\_ _ {} {} -> {}) tagName tagConstructor customBuilder tag0s
 
 
+tag0Maker : (b -> c -> a -> d -> e) -> b -> c -> ( f, a ) -> ( b -> c -> f -> g, d ) -> ( g, e )
 tag0Maker next tagName tagConstructor ( customBuilder, restCustomBuilders ) ( tag0, restTag0s ) =
     ( tag0 tagName tagConstructor customBuilder
     , next tagName tagConstructor restCustomBuilders restTag0s
     )
 
 
+doMakeTag1 : ((a -> b -> c -> {} -> {} -> {}) -> f -> g -> h -> i -> j -> k) -> f -> g -> h -> i -> j -> k
 doMakeTag1 tag1Maker_ tagName tagConstructor child1 customBuilder tag1s =
     tag1Maker_ (\_ _ _ {} {} -> {}) tagName tagConstructor child1 customBuilder tag1s
 
 
+tag1Maker : (b -> c -> a -> d -> e -> f) -> b -> c -> ( g, a ) -> ( h, d ) -> ( b -> c -> g -> h -> i, e ) -> ( i, f )
 tag1Maker next tagName tagConstructor ( child1, restChild1s ) ( customBuilder, restCustomBuilders ) ( tag1, restTag1s ) =
     ( tag1 tagName tagConstructor child1 customBuilder
     , next tagName tagConstructor restChild1s restCustomBuilders restTag1s
     )
 
 
+doEndCustom : (({} -> {} -> {}) -> c -> d -> e) -> c -> d -> e
 doEndCustom customEnder_ customBuilder endCustoms =
     customEnder_ (\{} {} -> {}) customBuilder endCustoms
 
 
+customEnder : (b -> a -> c) -> ( d, b ) -> ( d -> e, a ) -> ( e, c )
 customEnder next ( customBuilder, restCustomBuilders ) ( endCustom, restEndCustoms ) =
     ( endCustom customBuilder
     , next restCustomBuilders restEndCustoms
     )
 
 
+doConstructMultiTool : ((a -> {} -> a) -> c -> d -> e) -> c -> d -> e
 doConstructMultiTool constructMultiTool_ ctor builder =
     constructMultiTool_ (\output {} -> output) ctor builder
 
 
+constructMultiTool : (a -> b -> c) -> (d -> a) -> ( d, b ) -> c
 constructMultiTool next ctor ( builder, restBuilders ) =
     next (ctor builder) restBuilders
