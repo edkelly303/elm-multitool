9e8dcf2161de6345bbb5ffc4e30753d66bbac001
Ed Kelly <edwardjkelly@gmail.com>
2024-07-01 11:46:58 +0100

    Add a test to check that all built-in ToolSpecs compile successfully

--- tests/Example.elm
+++ tests/Example.elm
@@ -1,10 +1,121 @@
 module Example exposing (..)
 
-import Expect exposing (Expectation)
-import Fuzz exposing (Fuzzer, int, list, string)
+import Control
+import Expect
+import MultiTool
 import Test exposing (..)
+import Tools.Control
+
+
+type alias Tools control =
+    { control : control }
+
+
+t =
+    MultiTool.define Tools Tools
+        |> MultiTool.add .control Tools.Control.interface
+        |> MultiTool.end
+
+
+type alias All string int bool float char list maybe array dict set tuple triple result custom =
+    { string : string
+    , int : int
+    , bool : bool
+    , float : float
+    , char : char
+    , list : list
+    , maybe : maybe
+    , array : array
+    , dict : dict
+    , set : set
+    , tuple : tuple
+    , triple : triple
+    , result : result
+    , custom : custom
+    }
+
+
+allSpec =
+    t.record All
+        |> t.field "string" .string t.string
+        |> t.field "int" .int t.int
+        |> t.field "bool" .bool t.bool
+        |> t.field "float" .float t.float
+        |> t.field "char" .char t.char
+        |> t.field "list" .list (t.list t.int)
+        |> t.field "maybe" .maybe (t.maybe t.int)
+        |> t.field "array" .array (t.array t.int)
+        |> t.field "dict" .dict (t.dict t.int t.string)
+        |> t.field "set" .set (t.set t.int)
+        |> t.field "tuple" .tuple (t.tuple t.int t.string)
+        |> t.field "triple" .triple (t.triple t.int t.string t.float)
+        |> t.field "result" .result (t.result t.int t.string)
+        |> t.field "custom"
+            .custom
+            (let
+                match =
+                    \a b c d e f tag ->
+                        case tag of
+                            A ->
+                                a
+
+                            B i ->
+                                b i
+
+                            C i1 i2 ->
+                                c i1 i2
+
+                            D i1 i2 i3 ->
+                                d i1 i2 i3
+
+                            E i1 i2 i3 i4 ->
+                                e i1 i2 i3 i4
+
+                            F i1 i2 i3 i4 i5 ->
+                                f i1 i2 i3 i4 i5
+             in
+             t.customType { control = match }
+                |> t.variant0 "a" A
+                |> t.variant1 "b" B t.int
+                |> t.variant2 "c" C t.int t.int
+                |> t.variant3 "d" D t.int t.int t.int
+                |> t.variant4 "e" E t.int t.int t.int t.int
+                |> t.variant5 "f" F t.int t.int t.int t.int t.int
+                |> t.endCustomType
+            )
+        |> t.endRecord
+
+
+type Custom
+    = A
+    | B Int
+    | C Int Int
+    | D Int Int Int
+    | E Int Int Int Int
+    | F Int Int Int Int Int
+
+
+control =
+    t.build allSpec
+        |> .control
 
 
 suite : Test
 suite =
-    todo "Implement our first test. See https://package.elm-lang.org/packages/elm-explorations/test/latest for how to do this!"
+    test "all built-in ToolSpecs compile" <|
+        \_ ->
+            let
+                form =
+                    Control.simpleForm
+                        { control = control
+                        , onUpdate = Just
+                        , onSubmit = Nothing
+                        }
+
+                ( state, _ ) =
+                    form.blank
+
+                ( _, output ) =
+                    form.submit state
+            in
+            Expect.err output
---
+++ tests/Tools/Control.elm
@@ -0,0 +1,33 @@
+module Tools.Control exposing (interface)
+
+import Control
+
+
+interface =
+    { string = Control.string
+    , int = Control.int
+    , bool = Control.bool
+    , float = Control.float
+    , char = Control.char
+    , list = Control.list
+    , maybe = Control.maybe
+    , array = Control.array
+    , dict = Control.dict
+    , set = Control.set
+    , tuple = Control.tuple
+    , triple = Control.triple
+    , result = Control.result
+    , record = Control.record
+    , field =
+        -- Control.field doesn't use the field name
+        \_ getter subControl -> Control.field getter subControl
+    , endRecord = Control.endRecord
+    , customType = Control.customType
+    , variant0 = Control.variant0
+    , variant1 = Control.variant1
+    , variant2 = Control.variant2
+    , variant3 = Control.variant3
+    , variant4 = Control.variant4
+    , variant5 = Control.variant5
+    , endCustomType = Control.endCustomType
+    }
